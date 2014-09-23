722e6e6c3f9eb6fa83c073dada4a0bb00d3c81d7
ton.yeung <tyeung@auto-master.com>
2014-09-23 14:01:27 -0500

    initial commit

---
+++ MainSolution/Program.cs
@@ -0,0 +1,17 @@
+﻿using LibSolution;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainSolution
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myLibClass = new Class1();
+        }
+    }
+}
